088fbd857ce81112cf57b3ecc9566e6197f8a7c0
Paul De Silva <p.desilva@klab.com>
2014-10-24 14:21:32 +0800

    F. Activiy Part I / XML

---
+++ P0051_LayoutFiles/gen/ru/startandroid/develop/LayoutFiles/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ru.startandroid.develop.LayoutFiles;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
