c6f5f428f91dea754b3be12375132b74c3ebffd7
Paul De Silva <p.desilva@klab.com>
2014-10-27 15:18:10 +0800

    Input controls

--- InputControls/src/com/android/developer/inputcontrols/InputControlsActivity.java
+++ InputControls/src/com/android/developer/inputcontrols/InputControlsActivity.java
@@ -5,24 +5,65 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+
 
 public class InputControlsActivity extends ActionBarActivity {
 
+	CheckBox cbkEveryday;
+	CheckBox cbkWeekday;
+	CheckBox cbkWeekend;
+	Spinner mSnooze;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_input_controls);
 		
+		cbkEveryday = (CheckBox) findViewById(R.id.checkBox1);
+		cbkWeekday = (CheckBox) findViewById(R.id.checkBox2);
+		cbkWeekend = (CheckBox) findViewById(R.id.checkBox3);
+		mSnooze = (Spinner) findViewById(R.id.spinner1);
+		
+		final EditText edittext = (EditText) findViewById(R.id.editText1);
 		Button button = (Button) findViewById(R.id.button1);
 		button.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(getApplication(), "Bomb has been planted.", Toast.LENGTH_SHORT).show();
+				Toast.makeText(getApplication(), edittext.getText().toString() + "Alarm has been set", Toast.LENGTH_SHORT).show();
 				}
 		});
+		
+		
+		
+		cbkEveryday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if(cbkEveryday.isChecked()==true){
+					cbkWeekday.setEnabled(false);
+					cbkWeekend.setEnabled(false);
+				} else {
+					cbkWeekday.setEnabled(true);
+					cbkWeekend.setEnabled(true);
+				}
+				
+			}
+		});
+		
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,  R.array.snooze_time_array, android.R.layout.simple_spinner_dropdown_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mSnooze.setAdapter(adapter);
+		
+		
 	}
 
 	@Override
