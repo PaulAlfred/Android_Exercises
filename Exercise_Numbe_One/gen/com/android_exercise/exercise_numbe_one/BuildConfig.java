9a460b1d79f0ebb639e4f3d9619115ec0435d768
Paul De Silva <p.desilva@klab.com>
2014-10-27 15:46:41 +0800

    Exercise One

---
+++ Exercise_Numbe_One/gen/com/android_exercise/exercise_numbe_one/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.android_exercise.exercise_numbe_one;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
